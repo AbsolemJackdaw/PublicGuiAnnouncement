1bf8f133ae77d13a567820dde75c48ec81b0af98
Subaraki <robbeenaxel@hotmail.com>
2020-12-03 19:33:39 +0100

    1.16 update moved the renderer a bit around to accommodate 1.16 changes (private fields) moevd FlatBox to ModelFlat inside renderer

--- src/main/java/subaraki/pga/render/layer/LayerScreen.java
+++ src/main/java/subaraki/pga/render/layer/LayerScreen.java
@@ -1,19 +1,25 @@
 package subaraki.pga.render.layer;
 
-import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.player.AbstractClientPlayerEntity;
 import net.minecraft.client.gui.screen.inventory.CreativeScreen;
 import net.minecraft.client.gui.screen.inventory.InventoryScreen;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.PlayerRenderer;
 import net.minecraft.client.renderer.entity.layers.LayerRenderer;
 import net.minecraft.client.renderer.entity.model.PlayerModel;
 import net.minecraft.client.renderer.model.Model;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.vector.Vector3f;
 import subaraki.pga.capability.ScreenData;
 
 public class LayerScreen extends LayerRenderer<AbstractClientPlayerEntity, PlayerModel<AbstractClientPlayerEntity>> {
+
+    private ModelScreen model;
 
     public LayerScreen(PlayerRenderer renderer) {
 
@@ -21,59 +27,81 @@ public class LayerScreen extends LayerRenderer<AbstractClientPlayerEntity, Playe
     }
 
     @Override
-    public boolean shouldCombineTextures() {
-
-        return false;
-    }
-
-    @Override
-    public void render(AbstractClientPlayerEntity entityIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
+    public void render(MatrixStack mat, IRenderTypeBuffer bufferIn, int packedLightIn, AbstractClientPlayerEntity entityIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch)
+    {
 
         if (Minecraft.getInstance().currentScreen instanceof InventoryScreen || Minecraft.getInstance().currentScreen instanceof CreativeScreen)
             return;
 
         ScreenData.get(entityIn).ifPresent(data -> {
 
-            if (data != null && data.getViewingScreen() != null) {
-
-                // if (System.currentTimeMillis() % 60 == 0) {
-                // ScreenMod.LOG.debug(data.getViewingScreen().getResLoc() + " " +
-                // entityIn.getDisplayName().getFormattedText());
-                // }
+            if (data != null && data.getViewingScreen() != null)
+            {
 
                 ResourceLocation resLoc = data.lookupResloc(data.getViewingScreen().getRefName());
 
-                if (resLoc != null) {
+                if (resLoc != null)
+                {
 
-                    double pixelScale = 0.0625;
-                    int x = data.getViewingScreen().getSizeX();
-                    int y = data.getViewingScreen().getSizeY();
+                    float pixelScale = 0.0625F;
+                    int gui_size_x = data.getViewingScreen().getSizeX();
+                    int gui_size_y = data.getViewingScreen().getSizeY();
 
-                    Model m = new Model();
-                    m.textureWidth = data.getViewingScreen().getTexX();
-                    m.textureHeight = data.getViewingScreen().getTexY();
+                    int texture_size_x = data.getViewingScreen().getTexX();
+                    int texture_size_y = data.getViewingScreen().getTexY();
 
-                    RendererModelFlat model = new RendererModelFlat(m).addBox(0, 0, 0, x, y, 0);
+                    model = new ModelScreen(gui_size_x, gui_size_y, texture_size_x, texture_size_y);
 
-                    GlStateManager.pushMatrix();
+                    mat.push();
 
-                    GlStateManager.rotated(netHeadYaw, 0, 1, 0);
-                    GlStateManager.rotated(headPitch, 1, 0, 0);
+                    //thanks to Its_Meow from the MMD server for this rotation aid !
+                    mat.translate(this.getEntityModel().bipedHead.rotationPointX / 16.0F, this.getEntityModel().bipedHead.rotationPointY / 16.0F,
+                            this.getEntityModel().bipedHead.rotationPointZ / 16.0F);
+                    mat.rotate(Vector3f.ZP.rotation(this.getEntityModel().bipedHead.rotateAngleZ));
+                    mat.rotate(Vector3f.YP.rotation(this.getEntityModel().bipedHead.rotateAngleY));
+                    mat.rotate(Vector3f.XP.rotation(this.getEntityModel().bipedHead.rotateAngleX));
 
-                    double dx = ((double) x / 2.0) * pixelScale;
-                    double dy = ((double) y / 2.0) * pixelScale;
+                    double dx = ((double) gui_size_x / 2.0) * pixelScale;
+                    double dy = ((double) gui_size_y / 2.0) * pixelScale;
                     double headToCenterOffset = pixelScale * 4;
 
-                    GlStateManager.translated(-dx * 0.0625, -dy * 0.0625 - headToCenterOffset, -pixelScale * 16);
+                    mat.translate(-dx * 0.0625, -dy * 0.0625 - headToCenterOffset, -pixelScale * 16);
 
-                    GlStateManager.scaled(pixelScale, pixelScale, pixelScale);
+                    mat.scale(pixelScale, pixelScale, pixelScale);
 
-                    bindTexture(resLoc);
-                    model.render((float) pixelScale);
+                    IVertexBuilder ivertexbuilder = bufferIn.getBuffer(RenderType.getEntityCutout(resLoc));
+                    model.render(mat, ivertexbuilder, 0xffffff, 0xffffff, 1f, 1f, 1f, 1f);
 
-                    GlStateManager.popMatrix();
+                    mat.pop();
+
                 }
             }
         });
+
+    }
+
+    private static class ModelScreen extends Model {
+
+        final RendererModelFlat renderer;
+
+        public ModelScreen(int width, int height, int offx, int offy) {
+
+            super(RenderType::getEntityCutout);
+
+            this.textureHeight = offx;
+            this.textureWidth = offy;
+
+            renderer = new RendererModelFlat(this);
+            renderer.setTextureSize(offx, offy);
+            renderer.addBox(width, height);
+
+        }
+
+        @Override
+        public void render(MatrixStack matrixStackIn, IVertexBuilder bufferIn, int packedLightIn, int packedOverlayIn, float red, float green, float blue, float alpha)
+        {
+
+            renderer.render(matrixStackIn, bufferIn, packedLightIn, packedOverlayIn, red, green, blue, alpha);
+        }
     }
 }
--- src/main/java/subaraki/pga/render/layer/RendererModelFlat.java
+++ src/main/java/subaraki/pga/render/layer/RendererModelFlat.java
@@ -1,30 +1,286 @@
 package subaraki.pga.render.layer;
 
-import net.minecraft.client.renderer.entity.model.RendererModel;
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import it.unimi.dsi.fastutil.objects.ObjectList;
 import net.minecraft.client.renderer.model.Model;
+import net.minecraft.client.renderer.model.ModelRenderer;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.vector.Matrix3f;
+import net.minecraft.util.math.vector.Matrix4f;
+import net.minecraft.util.math.vector.Vector3f;
+import net.minecraft.util.math.vector.Vector4f;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
-public class RendererModelFlat extends RendererModel {
+@OnlyIn(Dist.CLIENT)
+public class RendererModelFlat extends ModelRenderer {
 
+    private float textureWidth = 0.0F;
+    private float textureHeight = 0.0F;
     private int textureOffsetX;
     private int textureOffsetY;
+    public float rotationPointX;
+    public float rotationPointY;
+    public float rotationPointZ;
+    public float rotateAngleX;
+    public float rotateAngleY;
+    public float rotateAngleZ;
+    public boolean mirror;
+    public boolean showModel = true;
+    private final ObjectList<ModelFlat> cubeList = new ObjectArrayList<>();
 
     public RendererModelFlat(Model model) {
 
         super(model);
     }
 
-    @Override
-    public RendererModelFlat addBox(float offX, float offY, float offZ, int width, int height, int depth) {
+    public RendererModelFlat(Model model, int texOffX, int texOffY) {
 
-        this.cubeList.add(new FlatBox(this, this.textureOffsetX, this.textureOffsetY, offX, offY, offZ, width, height, depth, 0.0F));
+        this(model.textureWidth, model.textureHeight, texOffX, texOffY);
+        model.accept(this);
+    }
+
+    public RendererModelFlat(int textureWidthIn, int textureHeightIn, int textureOffsetXIn, int textureOffsetYIn) {
+
+        super(textureWidthIn, textureHeightIn, textureOffsetXIn, textureOffsetYIn);
+    }
+
+    public ModelRenderer setTextureOffset(int x, int y)
+    {
+
+        this.textureOffsetX = x;
+        this.textureOffsetY = y;
         return this;
     }
 
+    public void addBox(int width, int height)
+    {
+
+        this.addBox(this.textureOffsetX, this.textureOffsetY, 0, 0, 0, width, height, 0, 0, 0, 0, false, false);
+    }
+
+    private void addBox(int texOffX, int texOffY, float x, float y, float z, float width, float height, float depth, float deltaX, float deltaY, float deltaZ, boolean mirorIn, boolean p_228305_13_)
+    {
+
+        this.cubeList
+                .add(new ModelFlat(texOffX, texOffY, x, y, z, width, height, depth, deltaX, deltaY, deltaZ, mirorIn, this.textureWidth, this.textureHeight));
+    }
+
     @Override
-    public RendererModelFlat setTextureSize(int textureWidthIn, int textureHeightIn) {
+    public void render(MatrixStack matrixStackIn, IVertexBuilder bufferIn, int packedLightIn, int packedOverlayIn)
+    {
+
+        this.render(matrixStackIn, bufferIn, packedLightIn, packedOverlayIn, 1.0F, 1.0F, 1.0F, 1.0F);
+    }
+
+    @Override
+    public void render(MatrixStack matrixStackIn, IVertexBuilder bufferIn, int packedLightIn, int packedOverlayIn, float red, float green, float blue, float alpha)
+    {
+
+        if (this.showModel)
+        {
+            if (!this.cubeList.isEmpty())
+            {
+                matrixStackIn.push();
+                this.translateRotate(matrixStackIn);
+                this.doRender(matrixStackIn.getLast(), bufferIn, packedLightIn, packedOverlayIn, red, green, blue, alpha);
+
+                matrixStackIn.pop();
+            }
+        }
+    }
+
+    @Override
+    public void translateRotate(MatrixStack matrixStackIn)
+    {
+
+        matrixStackIn.translate((double) (this.rotationPointX / 16.0F), (double) (this.rotationPointY / 16.0F), (double) (this.rotationPointZ / 16.0F));
+        if (this.rotateAngleZ != 0.0F)
+        {
+            matrixStackIn.rotate(Vector3f.ZP.rotation(this.rotateAngleZ));
+        }
+
+        if (this.rotateAngleY != 0.0F)
+        {
+            matrixStackIn.rotate(Vector3f.YP.rotation(this.rotateAngleY));
+        }
+
+        if (this.rotateAngleX != 0.0F)
+        {
+            matrixStackIn.rotate(Vector3f.XP.rotation(this.rotateAngleX));
+        }
+
+    }
+
+    private void doRender(MatrixStack.Entry matrixEntryIn, IVertexBuilder bufferIn, int packedLightIn, int packedOverlayIn, float red, float green, float blue, float alpha)
+    {
+
+        Matrix4f matrix4f = matrixEntryIn.getMatrix();
+        Matrix3f matrix3f = matrixEntryIn.getNormal();
+
+        for (ModelFlat modelrenderer$modelbox : this.cubeList)
+        {
+            for (OneTexturedQuad the_quad : modelrenderer$modelbox.quads)
+            {
+                Vector3f vector3f = the_quad.normal.copy();
+                vector3f.transform(matrix3f);
+                float f = vector3f.getX();
+                float f1 = vector3f.getY();
+                float f2 = vector3f.getZ();
+
+                for (int i = 0; i < 4; ++i)
+                {
+                    FlatPositionTextureVertex modelrenderer$positiontexturevertex = the_quad.vertexPositions[i];
+                    float f3 = modelrenderer$positiontexturevertex.position.getX() / 16.0F;
+                    float f4 = modelrenderer$positiontexturevertex.position.getY() / 16.0F;
+                    float f5 = modelrenderer$positiontexturevertex.position.getZ() / 16.0F;
+                    Vector4f vector4f = new Vector4f(f3, f4, f5, 1.0F);
+                    vector4f.transform(matrix4f);
+                    bufferIn.addVertex(vector4f.getX(), vector4f.getY(), vector4f.getZ(), red, green, blue, alpha, modelrenderer$positiontexturevertex.textureU,
+                            modelrenderer$positiontexturevertex.textureV, packedOverlayIn, packedLightIn, f, f1, f2);
+                }
+            }
+
+        }
+
+    }
+
+    /**
+     * Returns the model renderer with the new texture parameters.
+     */
+    public ModelRenderer setTextureSize(int textureWidthIn, int textureHeightIn)
+    {
 
         this.textureWidth = (float) textureWidthIn;
         this.textureHeight = (float) textureHeightIn;
         return this;
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public static class ModelFlat {
+
+        private final OneTexturedQuad quads[];
+        public final float posX1;
+        public final float posY1;
+        public final float posZ1;
+        public final float posX2;
+        public final float posY2;
+        public final float posZ2;
+
+        public ModelFlat(int texOffX, int texOffY, float x, float y, float z, float width, float height, float depth, float deltaX, float deltaY, float deltaZ, boolean mirorIn, float texWidth, float texHeight) {
+
+            this.posX1 = x;
+            this.posY1 = y;
+            this.posZ1 = z;
+            this.posX2 = x + width;
+            this.posY2 = y + height;
+            this.posZ2 = z + depth;
+            this.quads = new OneTexturedQuad[2];
+            float f = x + width;
+            float f1 = y + height;
+            float f2 = z + depth;
+            x = x - deltaX;
+            y = y - deltaY;
+            z = z - deltaZ;
+            f = f + deltaX;
+            f1 = f1 + deltaY;
+            f2 = f2 + deltaZ;
+            if (mirorIn)
+            {
+                float f3 = f;
+                f = x;
+                x = f3;
+            }
+
+            FlatPositionTextureVertex positiontexturevertex7 = new FlatPositionTextureVertex(x, y, z, 0.0F, 0.0F);
+            FlatPositionTextureVertex positiontexturevertex = new FlatPositionTextureVertex(f, y, z, 0.0F, 8.0F);
+            FlatPositionTextureVertex positiontexturevertex1 = new FlatPositionTextureVertex(f, f1, z, 8.0F, 8.0F);
+            FlatPositionTextureVertex positiontexturevertex2 = new FlatPositionTextureVertex(x, f1, z, 8.0F, 0.0F);
+            FlatPositionTextureVertex positiontexturevertex3 = new FlatPositionTextureVertex(x, y, f2, 0.0F, 0.0F);
+            FlatPositionTextureVertex positiontexturevertex4 = new FlatPositionTextureVertex(f, y, f2, 0.0F, 8.0F);
+            FlatPositionTextureVertex positiontexturevertex5 = new FlatPositionTextureVertex(f, f1, f2, 8.0F, 8.0F);
+            FlatPositionTextureVertex positiontexturevertex6 = new FlatPositionTextureVertex(x, f1, f2, 8.0F, 0.0F);
+            float f5 = (float) texOffX + depth;
+            float f6 = (float) texOffX + depth + width;
+            float f10 = (float) texOffY;
+            float f11 = (float) texOffY + depth;
+            float f12 = (float) texOffY + depth + height;
+            this.quads[0] =
+
+                    new OneTexturedQuad(
+                            new FlatPositionTextureVertex[] { positiontexturevertex3, positiontexturevertex4, positiontexturevertex5, positiontexturevertex6 },
+                            f5, f10, f6, f12, texWidth, texHeight, false, Direction.SOUTH);
+
+            this.quads[1] =
+
+                    new OneTexturedQuad(
+                            new FlatPositionTextureVertex[] { positiontexturevertex, positiontexturevertex7, positiontexturevertex2, positiontexturevertex1 },
+                            f6, f11, f5, f12, texWidth, texHeight, false, Direction.NORTH);
+
+        }
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    static class FlatPositionTextureVertex {
+
+        public final Vector3f position;
+        public final float textureU;
+        public final float textureV;
+
+        public FlatPositionTextureVertex(float x, float y, float z, float texU, float texV) {
+
+            this(new Vector3f(x, y, z), texU, texV);
+        }
+
+        public FlatPositionTextureVertex setTextureUV(float texU, float texV)
+        {
+
+            return new FlatPositionTextureVertex(this.position, texU, texV);
+        }
+
+        public FlatPositionTextureVertex(Vector3f posIn, float texU, float texV) {
+
+            this.position = posIn;
+            this.textureU = texU;
+            this.textureV = texV;
+        }
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    static class OneTexturedQuad {
+
+        public final FlatPositionTextureVertex[] vertexPositions;
+        public final Vector3f normal;
+
+        public OneTexturedQuad(FlatPositionTextureVertex[] positionsIn, float u1, float v1, float u2, float v2, float texWidth, float texHeight, boolean mirrorIn, Direction directionIn) {
+
+            this.vertexPositions = positionsIn;
+            float f = 0.0F / texWidth;
+            float f1 = 0.0F / texHeight;
+            positionsIn[0] = positionsIn[0].setTextureUV(u2 / texWidth - f, v1 / texHeight + f1);
+            positionsIn[1] = positionsIn[1].setTextureUV(u1 / texWidth + f, v1 / texHeight + f1);
+            positionsIn[2] = positionsIn[2].setTextureUV(u1 / texWidth + f, v2 / texHeight - f1);
+            positionsIn[3] = positionsIn[3].setTextureUV(u2 / texWidth - f, v2 / texHeight - f1);
+            if (mirrorIn)
+            {
+                int i = positionsIn.length;
+
+                for (int j = 0; j < i / 2; ++j)
+                {
+                    FlatPositionTextureVertex modelrenderer$positiontexturevertex = positionsIn[j];
+                    positionsIn[j] = positionsIn[i - 1 - j];
+                    positionsIn[i - 1 - j] = modelrenderer$positiontexturevertex;
+                }
+            }
+
+            this.normal = directionIn.toVector3f();
+            if (mirrorIn)
+            {
+                this.normal.mul(-1.0F, 1.0F, 1.0F);
+            }
+        }
     }
 }
