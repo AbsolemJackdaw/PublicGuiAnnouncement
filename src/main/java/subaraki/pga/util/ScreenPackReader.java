d75f01e9a3eef11a9ba96d82e3e41c98f3ae2162
Subaraki <robbeenaxel@hotmail.com>
2021-08-05 15:57:28 +0200

    implement 1.17 resource reload manager system

--- src/main/java/subaraki/pga/util/ScreenPackReader.java
+++ src/main/java/subaraki/pga/util/ScreenPackReader.java
@@ -1,5 +1,19 @@
 package subaraki.pga.util;
 
+import com.google.common.collect.Lists;
+import com.google.gson.*;
+import net.minecraft.client.Minecraft;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.Resource;
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.server.packs.resources.SimplePreparableReloadListener;
+import net.minecraft.util.profiling.ProfilerFiller;
+import net.minecraftforge.client.event.RegisterClientReloadListenersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import subaraki.pga.mod.ScreenMod;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,125 +23,46 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-
-import net.minecraft.client.Minecraft;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.packs.resources.Resource;
-import net.minecraft.server.packs.resources.ResourceManagerReloadListener;
-import net.minecraftforge.event.AddReloadListenerEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
-import subaraki.pga.mod.ScreenMod;
-
-@EventBusSubscriber(modid = ScreenMod.MODID, bus = Bus.FORGE)
-public class ScreenPackReader {
+@EventBusSubscriber(modid = ScreenMod.MODID, bus = Bus.MOD)
+public class ScreenPackReader extends SimplePreparableReloadListener<ArrayList<JsonObject>> {
 
     private static HashMap<String, ScreenEntry> mappedScreens = new HashMap<>();
 
     @SubscribeEvent
-    public static void registerreloadListener(AddReloadListenerEvent event)
-    {
+    public static void registerReloadListener(RegisterClientReloadListenersEvent event) {
 
-        event.addListener((ResourceManagerReloadListener) ((resourceManager) -> {
-            loadFromJson();
-        }));
-
+        event.registerReloadListener(new ScreenPackReader());
     }
 
-    public ScreenPackReader init()
-    {
 
-        loadFromJson();
-        return this;
-    }
+    @Override
+    protected ArrayList<JsonObject> prepare(ResourceManager resourceManager, ProfilerFiller profilerFiller) {
 
-    private static void loadFromJson()
-    {
+        ArrayList<JsonObject> theJsonFiles = Lists.newArrayList();
+        try {
 
-        try
-        {
+            Collection<ResourceLocation> jsonfiles = resourceManager.listResources("load_screens", (filename) -> filename.endsWith(".json"));
 
-            Collection<ResourceLocation> jsonfiles = Minecraft.getInstance().getResourceManager().listResources("load_screens", (filename) -> {
-                return filename.endsWith(".json");
-            });
+            List<Resource> jsons = new ArrayList<>();
 
-            List<Resource> jsons = new ArrayList<Resource>();
-
-            for (ResourceLocation resLoc : jsonfiles)
-            {
+            for (ResourceLocation resLoc : jsonfiles) {
                 jsons.addAll(Minecraft.getInstance().getResourceManager().getResources(resLoc));
             }
 
             Gson gson = new GsonBuilder().create();
 
-            for (Resource res : jsons)
-            {
+            for (Resource res : jsons) {
                 InputStream stream = res.getInputStream();
 
                 BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
                 JsonElement je = gson.fromJson(reader, JsonElement.class);
                 JsonObject json = je.getAsJsonObject();
 
-                if (json.has("screens"))
-                {
-                    JsonArray array = json.getAsJsonArray("screens");
-                    for (int i = 0; i < array.size(); i++)
-                    {
-
-                        JsonObject jsonObject = array.get(i).getAsJsonObject();
-
-                        String fullName = jsonObject.get("class").getAsString();
-
-                        String path = jsonObject.get("texture").getAsString();
-
-                        int sizeX = 0;
-                        int sizeY = 0;
-                        int texX = 0;
-                        int texY = 0;
-
-                        if (jsonObject.has("size"))
-                        {
-                            JsonArray list = jsonObject.getAsJsonArray("size");
-                            if (list.size() == 2)
-                            {
-                                sizeX = list.get(0).getAsInt();
-                                sizeY = list.get(1).getAsInt();
-                            }
-                        }
-
-                        if (jsonObject.has("texSize"))
-                        {
-                            JsonArray list = jsonObject.getAsJsonArray("texSize");
-                            if (list.size() == 2)
-                            {
-                                texX = list.get(0).getAsInt();
-                                texY = list.get(1).getAsInt();
-                            }
-                        }
-
-                        if (jsonObject.has("fullSize"))
-                        {
-                            int size = jsonObject.get("fullSize").getAsInt();
-                            sizeX = sizeY = texX = texY = size;
-                        }
-
-                        ScreenEntry entry = new ScreenEntry(fullName, path, sizeX, sizeY, texX, texY);
-                        ScreenMod.LOG.debug(String.format("Loaded %s for %s : file size %d x %d , tex size %d x %d", entry.getResLoc(), entry.getRefName(),
-                                entry.getTexX(), entry.getTexY(), entry.getSizeX(), entry.getSizeY()));
-                        mappedScreens.put(entry.getRefName(), entry);
-
-                    }
+                if (json.has("screens")) {
+                    theJsonFiles.add(json);
                 }
             }
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             ScreenMod.LOG.warn("************************************");
             ScreenMod.LOG.warn("!*!*!*!*!");
             ScreenMod.LOG.warn("No Screens Detected. You will not be able to use ");
@@ -139,10 +74,65 @@ public class ScreenPackReader {
 
             e.printStackTrace();
         }
+        return theJsonFiles;
     }
 
-    public static ScreenEntry getEntryForSimpleClassName(String simpleclassname)
-    {
+    @Override
+    protected void apply(ArrayList<JsonObject> o, ResourceManager resourceManager, ProfilerFiller profilerFiller) {
+        if (o != null && !o.isEmpty()) {
+
+            Runnable run = () -> {
+                for (JsonObject json : o)
+                    if (json.has("screens")) {
+                        JsonArray array = json.getAsJsonArray("screens");
+                        for (int i = 0; i < array.size(); i++) {
+
+                            JsonObject jsonObject = array.get(i).getAsJsonObject();
+
+                            String fullName = jsonObject.get("class").getAsString();
+
+                            String path = jsonObject.get("texture").getAsString();
+
+                            int sizeX = 0;
+                            int sizeY = 0;
+                            int texX = 0;
+                            int texY = 0;
+
+                            if (jsonObject.has("size")) {
+                                JsonArray list = jsonObject.getAsJsonArray("size");
+                                if (list.size() == 2) {
+                                    sizeX = list.get(0).getAsInt();
+                                    sizeY = list.get(1).getAsInt();
+                                }
+                            }
+
+                            if (jsonObject.has("texSize")) {
+                                JsonArray list = jsonObject.getAsJsonArray("texSize");
+                                if (list.size() == 2) {
+                                    texX = list.get(0).getAsInt();
+                                    texY = list.get(1).getAsInt();
+                                }
+                            }
+
+                            if (jsonObject.has("fullSize")) {
+                                int size = jsonObject.get("fullSize").getAsInt();
+                                sizeX = sizeY = texX = texY = size;
+                            }
+
+                            ScreenEntry entry = new ScreenEntry(fullName, path, sizeX, sizeY, texX, texY);
+                            ScreenMod.LOG.info(String.format("Loaded %s for %s : file size %d x %d , tex size %d x %d", entry.getResLoc(), entry.getRefName(),
+                                    entry.getTexX(), entry.getTexY(), entry.getSizeX(), entry.getSizeY()));
+
+                            mappedScreens.put(entry.getRefName(), entry);
+
+                        }
+                    }
+            };
+            run.run();
+        }
+    }
+
+    public static ScreenEntry getEntryForSimpleClassName(String simpleclassname) {
 
         if (mappedScreens.containsKey(simpleclassname))
             return mappedScreens.get(simpleclassname);
