6eec15d10fd839048600975bdd29307c54ef5cde
Subaraki <robbeenaxel@hotmail.com>
2021-08-05 15:58:14 +0200

    Fixed potential NPE issues by checking if parent is null. Moved layer loading to its own event

--- src/main/java/subaraki/pga/util/ClientReferences.java
+++ src/main/java/subaraki/pga/util/ClientReferences.java
@@ -1,43 +1,30 @@
 package subaraki.pga.util;
 
-import java.util.UUID;
-
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.entity.player.PlayerRenderer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import subaraki.pga.mod.ScreenMod;
-import subaraki.pga.render.layer.LayerScreen;
+
+import java.util.UUID;
 
 public class ClientReferences {
 
-    public static Player getClientPlayer()
-    {
+    public static Player getClientPlayer() {
+        if (Minecraft.getInstance() == null)
+            return null;
 
         return Minecraft.getInstance().player;
     }
 
-    public static Level getClientWorld()
-    {
+    public static Level getClientWorld() {
+        if (Minecraft.getInstance() == null)
+            return null;
 
         return Minecraft.getInstance().level;
     }
 
-    public static Player getClientPlayerByUUID(UUID uuid)
-    {
+    public static Player getClientPlayerByUUID(UUID uuid) {
 
         return Minecraft.getInstance().level.getPlayerByUUID(uuid);
     }
 
-    public static void loadLayers()
-    {
-
-        String types[] = new String[] { "default", "slim" };
-        ScreenMod.LOG.info(Minecraft.getInstance().getEntityRenderDispatcher());
-        for (String type : types)
-        {
-            PlayerRenderer renderer = ((PlayerRenderer) Minecraft.getInstance().getEntityRenderDispatcher().getSkinMap().get(type));
-            renderer.addLayer(new LayerScreen(renderer));
-        }
-    }
 }
